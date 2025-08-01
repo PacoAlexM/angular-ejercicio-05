51d2f945601249d8c628cc7f0b5c23e72d4dd2c2
PacoAlexM <pako_ale_martell@hotmail.com>
2025-07-31 22:59:00 -0600

    Se agregan ejemplos de pipes de number y fechas

--- src/app/pages/basic/basic.component.ts
+++ src/app/pages/basic/basic.component.ts
@@ -1,13 +1,26 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
     selector: 'app-basic-page',
-    imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+    imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
     templateUrl: './basic.component.html'
 })
 export default class BasicComponent {
     nameLower = signal('francisco');
     nameUpper = signal('FRANCISCO');
     fullName = signal('FrANcIsCO ALEjanDro');
+
+    myDate = signal(new Date());
+
+    myTimerEffect = effect(onClear => {
+        const interval = setInterval(() => {
+            this.myDate.set(new Date());
+            console.log('tick');
+        }, 1000);
+
+        onClear(() => {
+            clearInterval(interval);
+        });
+    });
 }
--- src/app/pages/numbers/numbers.component.ts
+++ src/app/pages/numbers/numbers.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
     selector: 'app-numbers-page',
-    imports: [],
+    imports: [DecimalPipe, PercentPipe, CurrencyPipe],
     templateUrl: './numbers.component.html'
 })
-export default class NumbersComponent {}
+export default class NumbersComponent {
+    totalSales = signal(2_345_323.8746);
+    percent = signal(0.4856);
+}
