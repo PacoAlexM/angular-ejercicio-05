3d6e2b64844742b5be3a24223521840869f0ca6d
PacoAlexM <pako_ale_martell@hotmail.com>
2025-08-06 23:06:23 -0600

    Se crean pipes de i18nSelect

--- src/app/pages/uncommon/uncommon.component.ts
+++ src/app/pages/uncommon/uncommon.component.ts
@@ -1,8 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CardComponent } from "../../components/card/card.component";
+import { I18nSelectPipe } from '@angular/common';
+
+const cliente1 = {
+    name: 'Francisco',
+    gender: 'male',
+    age: 33,
+    address: 'Guadalajara, Mexico'
+}
+
+const cliente2 = {
+    name: 'Alejandra',
+    gender: 'female',
+    age: 23,
+    address: 'San Luis Potosi, Mexico'
+}
 
 @Component({
     selector: 'app-uncommon-page',
-    imports: [],
+    imports: [CardComponent, I18nSelectPipe],
     templateUrl: './uncommon.component.html'
 })
-export default class UncommonComponent {}
+export default class UncommonComponent {
+    // i18n Select
+    cliente = signal(cliente1);
+
+    invitationMap = {
+        male: 'invitarlo',
+        female: 'invitarla',
+    }
+
+    changeCliente() {
+        if (this.cliente() === cliente1) {
+            this.cliente.set(cliente2);
+            return;
+        }
+
+        this.cliente.set(cliente1);
+    }
+}
