0e2da72a0af4da22383b0d30483e36b90f1ed81e
PacoAlexM <pako_ale_martell@hotmail.com>
2025-08-13 23:06:50 -0600

    Creacion de pipes personalizados

--- src/app/pages/custom/custom.component.ts
+++ src/app/pages/custom/custom.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
+import { heroes } from '../../data/heroes.data';
+import { CanFlyPipe } from '../../pipes/can-fly.pipe';
 
 @Component({
     selector: 'app-custom-page',
-    imports: [],
+    imports: [ToggleCasePipe, CanFlyPipe],
     templateUrl: './custom.component.html'
 })
-export default class CustomComponent {}
+export default class CustomComponent {
+    name = signal('Francisco Alejandro');
+    toggle = signal(true);
+
+    heroes = signal(heroes);
+}
