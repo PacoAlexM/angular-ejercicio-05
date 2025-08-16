ba31bafa968bd54835f9a3b972f1c2ee198a8e47
PacoAlexM <pako_ale_martell@hotmail.com>
2025-08-16 01:01:36 -0600

    Creacion de pipes personalizados para ordenar y filtrar

--- src/app/pages/custom/custom.component.ts
+++ src/app/pages/custom/custom.component.ts
@@ -2,15 +2,24 @@ import { Component, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/heroes.data';
 import { CanFlyPipe } from '../../pipes/can-fly.pipe';
+import { HeroColorPipe } from '../../pipes/hero-color.pipe';
+import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
+import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
+import { TitleCasePipe } from '@angular/common';
+import { HeroSortByPipe } from '../../pipes/hero-sort-by.pipe';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroFilterPipe } from '../../pipes/hero-filter.pipe';
 
 @Component({
     selector: 'app-custom-page',
-    imports: [ToggleCasePipe, CanFlyPipe],
+    imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, HeroCreatorPipe, TitleCasePipe, HeroSortByPipe, HeroFilterPipe],
     templateUrl: './custom.component.html'
 })
 export default class CustomComponent {
     name = signal('Francisco Alejandro');
     toggle = signal(true);
-
     heroes = signal(heroes);
+    sortBy = signal<keyof Hero | null>(null);
+    searchQuery = signal('');
+    
 }
